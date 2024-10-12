3b6836d8a4bbdd41743dc3c3214debf9855ee312
Makwana Dainik  Kalabhai <dainikmakwana31@gmail.com>
2024-10-12 18:34:00 +0530

    msg

--- user_panel/header/search.php
+++ user_panel/header/search.php
@@ -12,9 +12,15 @@ if ($_POST["search_val"]!=null) {
         <div><a href='http://localhost/php/medicine_website/user_panel/shop/product_details/product_details.php?item_code=<?php echo $row["item_code"]; ?>&database=products'><i class='fa-solid fa-magnifying-glass'></i>&emsp;<?php echo $row["name"]; ?></a></div>
 <?php
     }
+
+    $select = $conn->prepare("SELECT * FROM `medicines` WHERE name LIKE '%" . $_POST["search_val"] . "%'");
+    $select->execute();
+
+    $select = $select->fetchAll();
+
+    foreach ($select as $row) {?>
+        <div><a href='http://localhost/php/medicine_website/user_panel/shop/product_details/product_details.php?item_code=<?php echo $row["item_code"]; ?>&database=medicines'><i class='fa-solid fa-magnifying-glass'></i>&emsp;<?php echo $row["name"]; ?></a></div>
+<?php
+    }
 }
-if (isset($_POST["search_input"])) {
-    unset($_SESSION["search_input"]);
-}
-exit;
 ?>
