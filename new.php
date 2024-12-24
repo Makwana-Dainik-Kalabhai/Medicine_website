274b7d17423a1b952243d49ec7dab4bbd60041ce
Makwana Dainik  Kalabhai <dainikmakwana31@gmail.com>
2024-12-24 22:30:43 +0530

    All categories of medicines are added successfully

--- new.php
+++ new.php
@@ -186,66 +186,59 @@ if (isset($_POST["sub"])) {
 <?php
 $conn = new PDO("mysql:host=localhost;dbname=medicine_website", "root", "");
 
-$cat = "Diabetes Care";
-$cat_img = "diabetes care.png";
+$cat = "Vitamins";
+$cat_img = "vitamins.jpeg";
 $item_img = [
-    "Kapiva Dia Free Juice 1 ltr(1).jpg",
-    "Kapiva Dia Free Juice 1 ltr(2).jpg",
-    "Kapiva Dia Free Juice 1 ltr(3).jpg",
-    "Kapiva Dia Free Juice 1 ltr(4).jpg",
-    "Kapiva Dia Free Juice 1 ltr(5).jpg",
-    "Kapiva Dia Free Juice 1 ltr(6).jpg",
-    "Kapiva Dia Free Juice 1 ltr(7).jpg",
-    "Kapiva Dia Free Juice 1 ltr(8).jpg",
-    "Kapiva Dia Free Juice 1 ltr(9).jpg"
+    "Revital H Capsule - Men 30s(1).jpg",
+    "Revital H Capsule - Men 30s(2).jpg",
+    "Revital H Capsule - Men 30s(3).jpg",
+    "Revital H Capsule - Men 30s(4).jpg",
+    "Revital H Capsule - Men 30s(5).jpg",
 ];
-$name = "Kapiva Dia Free Juice 1 ltr";
-$def = "Kapiva Dia Free Juice, 1L | Clinically Proven Blood Sugar Care | Amla, Karela, Jamun, Guduchi & 7 More";
-$off_price = 599;
-$price = 599;
-$dis = 0;
-$weight = "1 ltr";
+$name = "Revital H Capsule - Men 30s";
+$def = "evital H Multivitamin For Men (30 Capsules) With Natural Ginseng, Zinc, 10 Vitamins & 8 Minerals For Daily Energy, Stamina & Immunity";
+$off_price = 254.10;
+$price = 330;
+$dis = 23;
+$weight = "50 gm";
 $expiry = "Dec 2025";
 
 $des_img = [
-    "Kapiva Dia Free Juice 1 ltr(1).jpg",
-    "Kapiva Dia Free Juice 1 ltr(2).jpg",
-    "Kapiva Dia Free Juice 1 ltr(3).jpg",
-    "Kapiva Dia Free Juice 1 ltr(4).jpg",
+    "Revital H Capsule - Men 30s(1).jpg",
+    "Revital H Capsule - Men 30s(2).jpg",
+    "Revital H Capsule - Men 30s(3).jpg",
+    "Revital H Capsule - Men 30s(4).jpg",
+    "Revital H Capsule - Men 30s(5).jpg"
 ];
 
 $des = [
-    ["Kapiva Dia Free Juice, available in a generous 1-liter pack, is a carefully crafted formulation to support healthy blood sugar levels. This herbal juice combines the goodness of key ingredients such as Aloe Vera, Amla, and Gurmar, each known for its potential in promoting metabolic wellness. Aloe Vera, celebrated for its soothing properties, works in tandem with Amla, rich in antioxidants, to provide a nourishing foundation for overall well-being.Gurmar, an herb aptly named sugar destroyer, is a crucial component aimed at supporting balanced blood sugar levels. Kapiva Dia Free Juice offers a convenient and natural way to incorporate these Ayurvedic ingredients into your daily routine. Whether you are proactively managing blood sugar or seeking a holistic wellness elixir, this juice stands as a testament to Kapivas commitment to combining tradition with modern wellness needs."],
-    ["SCIENCE-BACKED 11 POTENT AYURVEDIC HERBS", "Kapiva Dia Free Juice is a blend of 11 Ayurvedic herbs that help in managing uncontrolled spikes in Blood sugar levels and eases out the complications of Blood Sugar such as improper digestion, low energy, and weak immunity."],
-    ["CONTROL BLOOD SUGAR LEVELS", "Herbs such as Karela, Amla, Jamun, Tulsi, Guduchi, Methi and Belpatra, optimize the production of hormones by breaking down glucose from foods. Regular consumption of this juice can help in maintaining optimal sugar levels within 3 months of consumption."],
-    ["100% AYURVEDIC", "This juice does not contain any added sugar or artificial taste-enhancers. It is 100% Ayurvedic and safe to be consumed along with your regular allopathic medications."],
-    ["NATURALLY SOURCED INGREDIENTS", "All ingredients are sourced from the choicest locations and mixed in the right proportions for maximum nutrition - Amla from Pratapgarh, Neem from Rajasthan, and so on. Each bottle of Dia Free Juice contains 45 Amlas, 24 Jamun Seeds, 1 Whole Karela and more - the best of herbs recommended in traditional texts to treat madhumeh."],
-    ["HOW TO CONSUME", "Mix 30ml Dia Free Juice with 30ml of water and consume it twice a day for long-term health benefits. If taken in the morning, should be taken on an empty stomach. Consume regularly for 3 Months for best results."]
+    ["Revital H multivitamin capsules for daily health is a balanced combination of Natural Ginseng, 10 Vitamins and 9 minerals which can help fill in nutritional gaps and support general well-being for a healthy, active lifestyle"],
+    ["Its key ingredients, Natural Ginseng, Vitamin B Complex and Iron, help support daily energy needs, and fight tiredness throughout the day"],
+    ["Daily essential vitamins, multivitamins and minerals such as vitamin C, vitamin D and Zinc help in improving immunity preventing frequent illness"],
+    ["Natural Ginseng and Magnesium, improve mental alertness and concentration, and increase the ability to manage stress"],
+    ["How to Use: 1 capsule daily with a glass of water/milk/juice"],
+    ["Boosts Energy, Improves Stamina and Supports Immunity"],
+    ["Ingredients","Vitamin A, B1, B2, B3, B6, B12, C, D and E, Folic Acid, Calcium, Phosphorous, Zinc, Iron, Magnesium, Potassium, Manganese, Copper, Iodine, Blend of Ginseng Root Extract"]
 ];
 $benefit = [
-    ["Gurmar, Aloe Vera, and Amla work synergistically to support healthy blood sugar levels."],
-    ["Amla, a potent antioxidant, contributes to overall well-being."],
-    ["Kapiva Dia Free Juice offers a convenient way to incorporate Ayurvedic ingredients into your daily routine."]
+    ["Centrum is Worlds no. 1 Multivitamin and Multimineral brand that is present globally across 7 continents and is now in India. Centrum Men is a daily nutritional supplement, to meet your modern-day nutrition needs so you can feel confident that your body can keep up with your demanding multi-life. Your daily supplement is formulated with 23 essential Vitamins and Minerals. Centrum formulations have been tailormade for Indian diets & body needs. Its comprehensive formula ensures overall health. The Immune support nutrients such as Vitamin C and Zinc enhances Immunity and helps in fighting off infections. The formulation contains blend of B Vitamins, Calcium and Magnesium that ensures muscle performance and also helps in strengthening of bones. Centrum Men is fortified with Botanical Blend of Grape Seed Extract to ensure healthy heart. Centrum Tablets are 100% vegetarian & suitable to consume for Vegans as well. These are Gluten free tablets with Non GMO. Do not worry, they are non habit forming. Centrum Multivitamin supplements are manufactured in a FSSAI certified facility that is GMP certified. Centrum is completely FSSAI Compliant. You can refer pack for correct dosage and administration, or can consult your health care practioner. For storage condition please refer the pack."]
 ];
 $how_use = [
-    ["Take 30 ml (two tablespoons) of Kapiva Dia Free Juice twice a day before meals or as directed by a healthcare professional."],
-    ["Adjust the dosage based on individual needs and consult with a professional for personalized advice."],
+    []
 ];
 $safety = [
-    ["Kapiva Dia Free Juice is generally safe for regular use. However, individuals with specific medical conditions, allergies, or those on medication should consult with a healthcare professional before using any new supplement. If any adverse reactions occur, discontinue use and seek professional advice."]
+    ["Store in a dry place away from heat and sunlight. Read the bottle label carefully before using it. Do not exceed the daily dosage limit, and take the capsule under your doctor’s supervision. Keep away from children."]
 ];
 $other_info = [
-    ["Keep out of reach of children and store in a cool, dry place, away from direct sunlight."]
+    []
 ];
 
 $faqs = [
-    ["Is Kapiva Dia Free Juice suitable for individuals with diabetes?", "Kapiva Dia Free Juice is specifically formulated to support healthy blood sugar levels. However, individuals with diabetes should consult with a healthcare professional before adding any new supplement to their routine."],
-    ["Can Kapiva Dia Free Juice replace medication for diabetes?", "No, Kapiva Dia Free Juice is a supplement and not a replacement for prescribed medications. Individuals with diabetes should continue their prescribed treatment and consult with a healthcare professional before making any changes."],
-    ["Are there any known side effects of Kapiva Dia Free Juice?", "The juice is generally well-tolerated, but if any adverse reactions occur, discontinue use and seek professional advice."]
+    []
 ];
 
 $delivery_date = '2024-12-30';
-$product_id = "1039";
+$product_id = "1045";
 
 $in = $conn->prepare("INSERT INTO `products` VALUES(NOW(), '$cat','$cat_img','" . serialize($item_img) . "','$name','$def','$off_price','$price','$dis','$weight','10','" . serialize($des_img) . "','" . serialize($des) . "','','','','$expiry','" . serialize($benefit) . "','" . serialize($how_use) . "','" . serialize($safety) . "','" . serialize($other_info) . "','" . serialize($faqs) . "','$delivery_date','$product_id','medicine')");
 // $in->execute();
